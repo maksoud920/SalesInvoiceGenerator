5e7970d9b19944cf6dd078b6340df2eba7164e97
Ahmed <ahmed_maksoud@example.com>
2022-12-24 21:24:29 +0200

    forth commit

--- src/controller/TestWriteMain.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import model.FileOperations;
-
-public class TestWriteMain {
-    public static void main(String[] s) {
-        FileOperations test = new FileOperations();
-    }
-}
